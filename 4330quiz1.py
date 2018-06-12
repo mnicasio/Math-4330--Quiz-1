1dfb3c0e5ea438d7c01cd739178aad111aa3d178
bdanzer <britt.danzer@gmail.com>
2018-06-11 19:22:34 -0500

    fixed functions to be cleaner

--- 4330quiz1.py
+++ 4330quiz1.py
@@ -1,16 +1,12 @@
-# matVec takes a matrix and a vector and returns the corresponding matrix-vector multiply.
-def matVec(matrix,vector):
-  #these variables are used as indices for matrix and vector multiplication
-  row = len(matrix)
-  col = len(vector)
-  #this is the empty vector where we will store our results
-  x =[] 
-  for i in range(row):
-    y=0
-    for j in range(col):
-      y += matrix[i][j]*vector[j]
-    x.append(y)
-  return x
+def inner_prod(v1, v2):
+  y = 0
+  for i in range(len(v1)):
+    y += v1[i] * v2[i]
+  return y
+
+def matmult3(m, v):
+  'matrix multiply vector by inner production.'
+  return [inner_prod(r, v) for r in m]
 
 # These are test variables being initialized to test the function matVec. Only one of these will give a correct answer the other ones should give error due to dimensions not matching up and due to wrong type of arg being used. 
 testMatrix01 = [[1,2],[3,4]]
@@ -22,8 +18,7 @@ testVector02 = [[5,6],[7,8]]
 testVector03 = 3
 
 # These are test cases for the function matVec. All but two of the tests should be commented out at a time so that we can see how each pair of inputs effects the output. 
-print(matVec(testMatrix01,testVector01))
+print(matmult3(testMatrix01,testVector01))
 #print(matVec(testMatrix02,testVector02))
 #print(matVec(testMatrix03,testVector03))
 #Added comment
-
